d0a196a7794602481088ead4e607beff201e4138
khanhndk <khanhndk@hcmup.edu.vn>
2017-12-22 14:52:01 +1100

    Add new/update setting functionality

--- AutoExcel/MainFrm.Designer.cs
+++ AutoExcel/MainFrm.Designer.cs
@@ -39,13 +39,18 @@
             this.txtSortAfter = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.chkSortAfterOrder = new System.Windows.Forms.CheckBox();
+            this.cbSetting = new System.Windows.Forms.ComboBox();
+            this.btnNewSetting = new System.Windows.Forms.Button();
+            this.btnUpdateSetting = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnProcess
             // 
-            this.btnProcess.Location = new System.Drawing.Point(1004, 650);
+            this.btnProcess.Location = new System.Drawing.Point(502, 338);
+            this.btnProcess.Margin = new System.Windows.Forms.Padding(2);
             this.btnProcess.Name = "btnProcess";
-            this.btnProcess.Size = new System.Drawing.Size(184, 50);
+            this.btnProcess.Size = new System.Drawing.Size(92, 26);
             this.btnProcess.TabIndex = 0;
             this.btnProcess.Text = "Process";
             this.btnProcess.UseVisualStyleBackColor = true;
@@ -54,17 +59,18 @@
             // lstFiles
             // 
             this.lstFiles.FormattingEnabled = true;
-            this.lstFiles.ItemHeight = 25;
-            this.lstFiles.Location = new System.Drawing.Point(32, 30);
+            this.lstFiles.Location = new System.Drawing.Point(16, 16);
+            this.lstFiles.Margin = new System.Windows.Forms.Padding(2);
             this.lstFiles.Name = "lstFiles";
-            this.lstFiles.Size = new System.Drawing.Size(935, 304);
+            this.lstFiles.Size = new System.Drawing.Size(470, 160);
             this.lstFiles.TabIndex = 1;
             // 
             // btnImport
             // 
-            this.btnImport.Location = new System.Drawing.Point(991, 30);
+            this.btnImport.Location = new System.Drawing.Point(496, 16);
+            this.btnImport.Margin = new System.Windows.Forms.Padding(2);
             this.btnImport.Name = "btnImport";
-            this.btnImport.Size = new System.Drawing.Size(184, 50);
+            this.btnImport.Size = new System.Drawing.Size(92, 26);
             this.btnImport.TabIndex = 2;
             this.btnImport.Text = "Import";
             this.btnImport.UseVisualStyleBackColor = true;
@@ -72,9 +78,10 @@
             // 
             // btnRemove
             // 
-            this.btnRemove.Location = new System.Drawing.Point(991, 117);
+            this.btnRemove.Location = new System.Drawing.Point(496, 61);
+            this.btnRemove.Margin = new System.Windows.Forms.Padding(2);
             this.btnRemove.Name = "btnRemove";
-            this.btnRemove.Size = new System.Drawing.Size(184, 50);
+            this.btnRemove.Size = new System.Drawing.Size(92, 26);
             this.btnRemove.TabIndex = 3;
             this.btnRemove.Text = "Remove";
             this.btnRemove.UseVisualStyleBackColor = true;
@@ -83,51 +90,57 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(27, 384);
+            this.label1.Location = new System.Drawing.Point(14, 200);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(228, 25);
+            this.label1.Size = new System.Drawing.Size(113, 13);
             this.label1.TabIndex = 4;
             this.label1.Text = "Sort By [before group]:";
             // 
             // txtSortBefore
             // 
-            this.txtSortBefore.Location = new System.Drawing.Point(259, 381);
+            this.txtSortBefore.Location = new System.Drawing.Point(130, 198);
+            this.txtSortBefore.Margin = new System.Windows.Forms.Padding(2);
             this.txtSortBefore.Name = "txtSortBefore";
-            this.txtSortBefore.Size = new System.Drawing.Size(100, 31);
+            this.txtSortBefore.Size = new System.Drawing.Size(52, 20);
             this.txtSortBefore.TabIndex = 5;
             this.txtSortBefore.Text = "B2";
             // 
             // txtGroupColumns
             // 
-            this.txtGroupColumns.Location = new System.Drawing.Point(259, 447);
+            this.txtGroupColumns.Location = new System.Drawing.Point(130, 232);
+            this.txtGroupColumns.Margin = new System.Windows.Forms.Padding(2);
             this.txtGroupColumns.Name = "txtGroupColumns";
-            this.txtGroupColumns.Size = new System.Drawing.Size(100, 31);
+            this.txtGroupColumns.Size = new System.Drawing.Size(52, 20);
             this.txtGroupColumns.TabIndex = 7;
-            this.txtGroupColumns.Text = "10, 15";
+            this.txtGroupColumns.Text = "10, 16";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(27, 450);
+            this.label2.Location = new System.Drawing.Point(14, 234);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(167, 25);
+            this.label2.Size = new System.Drawing.Size(82, 13);
             this.label2.TabIndex = 6;
             this.label2.Text = "Group Columns:";
             // 
             // txtSortAfter
             // 
-            this.txtSortAfter.Location = new System.Drawing.Point(259, 513);
+            this.txtSortAfter.Location = new System.Drawing.Point(130, 267);
+            this.txtSortAfter.Margin = new System.Windows.Forms.Padding(2);
             this.txtSortAfter.Name = "txtSortAfter";
-            this.txtSortAfter.Size = new System.Drawing.Size(100, 31);
+            this.txtSortAfter.Size = new System.Drawing.Size(52, 20);
             this.txtSortAfter.TabIndex = 9;
             this.txtSortAfter.Text = "J1";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(27, 516);
+            this.label3.Location = new System.Drawing.Point(14, 268);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(210, 25);
+            this.label3.Size = new System.Drawing.Size(104, 13);
             this.label3.TabIndex = 8;
             this.label3.Text = "Sort By [after group]:";
             // 
@@ -136,18 +149,63 @@
             this.chkSortAfterOrder.AutoSize = true;
             this.chkSortAfterOrder.Checked = true;
             this.chkSortAfterOrder.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.chkSortAfterOrder.Location = new System.Drawing.Point(23, 582);
+            this.chkSortAfterOrder.Location = new System.Drawing.Point(12, 303);
+            this.chkSortAfterOrder.Margin = new System.Windows.Forms.Padding(2);
             this.chkSortAfterOrder.Name = "chkSortAfterOrder";
-            this.chkSortAfterOrder.Size = new System.Drawing.Size(265, 29);
+            this.chkSortAfterOrder.Size = new System.Drawing.Size(135, 17);
             this.chkSortAfterOrder.TabIndex = 11;
             this.chkSortAfterOrder.Text = "Sort Ascent after group";
             this.chkSortAfterOrder.UseVisualStyleBackColor = true;
             // 
+            // cbSetting
+            // 
+            this.cbSetting.FormattingEnabled = true;
+            this.cbSetting.Location = new System.Drawing.Point(334, 197);
+            this.cbSetting.Name = "cbSetting";
+            this.cbSetting.Size = new System.Drawing.Size(152, 21);
+            this.cbSetting.TabIndex = 12;
+            this.cbSetting.SelectedIndexChanged += new System.EventHandler(this.cbSetting_SelectedIndexChanged);
+            this.cbSetting.TextChanged += new System.EventHandler(this.cbSetting_TextChanged);
+            // 
+            // btnNewSetting
+            // 
+            this.btnNewSetting.Location = new System.Drawing.Point(289, 229);
+            this.btnNewSetting.Name = "btnNewSetting";
+            this.btnNewSetting.Size = new System.Drawing.Size(77, 23);
+            this.btnNewSetting.TabIndex = 13;
+            this.btnNewSetting.Text = "New Setting";
+            this.btnNewSetting.UseVisualStyleBackColor = true;
+            this.btnNewSetting.Click += new System.EventHandler(this.btnNewSetting_Click);
+            // 
+            // btnUpdateSetting
+            // 
+            this.btnUpdateSetting.Location = new System.Drawing.Point(372, 229);
+            this.btnUpdateSetting.Name = "btnUpdateSetting";
+            this.btnUpdateSetting.Size = new System.Drawing.Size(114, 23);
+            this.btnUpdateSetting.TabIndex = 14;
+            this.btnUpdateSetting.Text = "Update setting";
+            this.btnUpdateSetting.UseVisualStyleBackColor = true;
+            this.btnUpdateSetting.Click += new System.EventHandler(this.btnUpdateSetting_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(261, 200);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Load setting:";
+            // 
             // MainFrm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1200, 712);
+            this.ClientSize = new System.Drawing.Size(600, 370);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnUpdateSetting);
+            this.Controls.Add(this.btnNewSetting);
+            this.Controls.Add(this.cbSetting);
             this.Controls.Add(this.chkSortAfterOrder);
             this.Controls.Add(this.txtSortAfter);
             this.Controls.Add(this.label3);
@@ -159,9 +217,11 @@
             this.Controls.Add(this.btnImport);
             this.Controls.Add(this.lstFiles);
             this.Controls.Add(this.btnProcess);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "MainFrm";
-            this.Text = "Form1";
+            this.Text = "Auto Excel";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainFrm_FormClosed);
+            this.Load += new System.EventHandler(this.MainFrm_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -180,6 +240,10 @@
         private System.Windows.Forms.TextBox txtSortAfter;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.CheckBox chkSortAfterOrder;
+        private System.Windows.Forms.ComboBox cbSetting;
+        private System.Windows.Forms.Button btnNewSetting;
+        private System.Windows.Forms.Button btnUpdateSetting;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- AutoExcel/MainFrm.cs
+++ AutoExcel/MainFrm.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Microsoft.Office.Interop.Excel;
 using System.IO;
+using System.Reflection;
 
 namespace AutoExcel
 {
@@ -98,6 +99,92 @@ namespace AutoExcel
             int k = lstFiles.SelectedIndex;
             filelist.RemoveAt(k);
             lstFiles.Items.RemoveAt(k);
+        }
+
+        string app_path;
+        string default_setting_filename;
+        private void MainFrm_Load(object sender, EventArgs e)
+        {
+            app_path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\";
+            default_setting_filename = app_path + "default.setting";
+            if (!File.Exists(default_setting_filename))
+            {
+                Save_Setting("default");
+            }
+            Load_Setting_List();
+            cbSetting.SelectedIndex = 0;
+        }
+
+        private void Load_Setting_List()
+        {
+            string[] setting_filenames = Directory.GetFiles(app_path, "*.setting");
+            cbSetting.Items.Clear();
+            foreach (string filename in setting_filenames)
+                cbSetting.Items.Add(Path.GetFileNameWithoutExtension(filename));
+        }
+        private void Load_Setting(string filename)
+        {
+            StreamReader reader = new StreamReader(filename);
+            List<string> settings = new List<string>();
+            while (!reader.EndOfStream)
+                settings.Add(reader.ReadLine());
+            reader.Close();
+            txtSortBefore.Text = settings[0];
+            txtGroupColumns.Text = settings[1];
+            txtSortAfter.Text = settings[2];
+        }
+
+        private void Save_Setting(string settingname)
+        {
+            List<string> settings = new List<string>();
+            settings.Add(txtSortBefore.Text);
+            settings.Add(txtGroupColumns.Text);
+            settings.Add(txtSortAfter.Text);
+            string setting_filename = app_path + settingname + ".setting";
+            StreamWriter writer = new StreamWriter(setting_filename);
+            for (int i = 0; i < settings.Count; i++)
+                writer.WriteLine(settings[i]);
+            writer.Close();
+        }
+
+        private void btnNewSetting_Click(object sender, EventArgs e)
+        {
+            if (cbSetting.Text.Trim() == "")
+                MessageBox.Show("Please input the new name setting", "Error");
+            Process_Save_Setting();
+        }
+
+        private void cbSetting_TextChanged(object sender, EventArgs e)
+        {
+            bool status_new = false;
+            if (cbSetting.SelectedItem == null)
+                status_new = true;
+            btnNewSetting.Enabled = status_new;
+            btnUpdateSetting.Enabled = !status_new;
+
+        }
+
+        private void btnUpdateSetting_Click(object sender, EventArgs e)
+        {
+            Process_Save_Setting();
+        }
+
+        private void Process_Save_Setting()
+        {
+            string current_setting = cbSetting.Text;
+            Save_Setting(current_setting);
+            Load_Setting_List();
+            for (int i = 0; i < cbSetting.Items.Count; i++)
+                if (cbSetting.Items[i].ToString() == current_setting)
+                    cbSetting.SelectedIndex = i;
+        }
+
+        private void cbSetting_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cbSetting.SelectedItem == null)
+                return;
+            string current_setting_filename = app_path + cbSetting.SelectedItem + ".setting";
+            Load_Setting(current_setting_filename);
         }
     }
 }
